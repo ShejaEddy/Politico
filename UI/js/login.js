dfe5e24cd3daad8ac9d1a014cba494e8f33a6e2d
shejaeddy <shejaeddy05@gmail.com>
2020-03-11 13:51:35 +0200

    feature(forgot-password): user is now able to reset their passwords

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,34 +1,59 @@
 const el = query => document.querySelector(query);
-el(".login-form").addEventListener("submit", e => {
-  e.preventDefault();
-  login();
-});
+if (el(".login-form"))
+  el(".login-form").addEventListener("submit", e => {
+    e.preventDefault();
+    login();
+  });
 const login = () => {
   var email = el("#email").value;
   var password = el("#password").value;
-  var taoster = el("#toaster");
+  var toaster = el("#toaster");
+  toaster.className = "show";
   if (email === "admin@example.com" && password == "password") {
-    toaster.className = "show";
     toaster.innerText = "Authenticated as Admin successfully";
     toaster.style.backgroundColor = "#10a567";
     setTimeout(function() {
-      taoster.className = taoster.className.replace("show", "");
+      toaster.className = toaster.className.replace("show", "");
       window.location.href = "parties.html";
     }, 3000);
   } else if (email === "user@example.com" && password == "password") {
-    toaster.className = "show";
     toaster.innerText = "Authenticated as User successfully";
     toaster.style.backgroundColor = "#10a567";
     setTimeout(function() {
-      taoster.className = taoster.className.replace("show", "");
+      toaster.className = toaster.className.replace("show", "");
       window.location.href = "vote.html";
     }, 3000);
   } else {
-    toaster.className = "show";
-    toaster.innerText = "Invali Email/Password, Please Try again";
+    toaster.innerText = "Invalid Email/Password, Please Try again";
     toaster.style.backgroundColor = "red";
     setTimeout(function() {
-      taoster.className = taoster.className.replace("show", "");
+      toaster.className = toaster.className.replace("show", "");
+    }, 3000);
+  }
+};
+
+if (el(".forgot-form"))
+  el(".forgot-form").addEventListener("submit", e => {
+    e.preventDefault();
+    forgotPwd();
+  });
+
+const forgotPwd = () => {
+  var email = el("#email").value;
+  var toaster = el("#toaster");
+  toaster.className = "show";
+  if (email === "admin@example.com" || email === "user@example.com") {
+    toaster.innerText = "Reset Link sent successfully, Check your emails!";
+    toaster.style.backgroundColor = "#10a567";
+    setTimeout(function() {
+      toaster.className = toaster.className.replace("show", "");
+      window.location.href = "login.html";
+    }, 3000);
+  } else {
+    toaster.innerText = "User doesn't exist, Please Try again";
+    toaster.style.backgroundColor = "red";
+    setTimeout(function() {
+      toaster.className = toaster.className.replace("show", "");
     }, 3000);
   }
 };
