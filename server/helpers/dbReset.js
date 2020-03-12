377af53a340d7daadca4478c28ec2b57ab3b3276
shejaeddy <shejaeddy05@gmail.com>
2020-03-12 16:21:09 +0200

    fix: removed misplaced file

--- server/helpers/dbReset.js
+++
@@ -1,5 +0,0 @@
-import { pool } from "../models";
-const query = `DROP TABLE IF EXISTS 
-                      user_info, office_tb, party_tb, petition_tb, vote_tb, candidate_tb`;
-
-export default async () => await pool.query(query);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes";
-require("./database");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
