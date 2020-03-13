c313e2740aa2d6bd7016884e4299af22ffe630cd
shejaeddy <shejaeddy05@gmail.com>
2020-03-13 05:36:19 +0200

    fix: style css

--- UI/js/petition.js
+++ UI/js/petition.js
@@ -1,26 +1,28 @@
-var modal = document.getElementById("myModal");
+const el = elm => document.querySelector(elm);
+var modal = el("#myModal");
 
-var btn = document.getElementById("myBtn");
+var btn = el("#myBtn");
 
-var span = document.getElementsByClassName("close")[0];
+var span = el(".close");
 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+btn.onclick = function() {
+  modal.style.display = "block";
+};
 
-span.onclick = function () {
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+el("#petition-form").addEventListener("submit", e => {
+  e.preventDefault();
+  el("#body").style.backgroundImage = "none";
+  el("#default-heading").innerText = "All Petitions";
+  modal.style.display = "none";
+  el(".table").classList.remove("d-none");
+});
+const openFile = () => el(".fileInput").click();
+window.onclick = function(event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-}
-const el = (elm) => document.querySelector(elm)
-el("#petition-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    el("#body").style.backgroundImage = "none";
-    el('#default-heading').innerText = "All Petitions";
-    modal.style.display = "none";
-    el(".table").classList.remove ("d-none");
-})
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  }
+};
