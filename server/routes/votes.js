321d17223132026c029a45944a91abd52c73313d
shejaeddy <shejaeddy05@gmail.com>
2020-03-17 18:43:19 +0200

    fix: voting tests

--- server/routes/votes.js
+++ server/routes/votes.js
@@ -3,14 +3,14 @@ import VoteControllers from "../controllers/votes";
 import validate from "../controllers/votes/validate";
 import authenticate from "../middleware/authenticate";
 import authorize from "../middleware/authorize";
-import { Admin } from "../helpers/roles";
+import { User } from "../helpers/roles";
 
 const votesRouters = Router();
 
 votesRouters.post(
   "/votes",
   authenticate,
-  authorize(Admin),
+  authorize(User),
   validate,
   VoteControllers.create
 );
