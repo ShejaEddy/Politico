32490b00fc08302cf53a6ef5f6767b1c3e4149ff
shejaeddy <shejaeddy05@gmail.com>
2020-03-17 20:03:29 +0200

    fix: resolve merge conflicts

--- server/controllers/votes/index.js
+++ server/controllers/votes/index.js
@@ -1,5 +1,5 @@
 import db from "../../database";
-import { Offices, Users } from "../../database/models";
+import { Offices, Users, Votes } from "../../database/models";
 import {
   badRequest,
   okResponse,
--- server/routes/votes.js
+++ server/routes/votes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 import VoteControllers from "../controllers/votes";
 import validate from "../controllers/votes/validate";
 import authenticate from "../middleware/authenticate";
-import authorize from "../middleware/authorize";
-import { User } from "../helpers/roles";
 
 const votesRouters = Router();
 votesRouters
