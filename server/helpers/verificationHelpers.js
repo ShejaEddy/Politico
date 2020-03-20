3762a87f9f6fc8e3a8b4fe6f275b42c726589645
shejaeddy <shejaeddy05@gmail.com>
2020-03-20 09:44:57 +0200

    fix: response from sendgrid

--- server/helpers/verificationHelpers.js
+++ server/helpers/verificationHelpers.js
@@ -10,18 +10,17 @@ sendGrid.setApiKey(SENDGRID_KEY);
 export default (res, to, data) => {
   const msg = {
     method: "POST",
-    path: "/v3/mail/send",
     to,
     from: "politico@gmail.com",
     subject: "Politico sent you a Reset Password token",
-    text: `Hello ${data.name}, <br> http://localhost:3000/api/v1/auth/reset_password?token=${data.token}&email=${to} You requested for a password reset click here
-       <button type="submit" style="background:black; color:white; padding:0.5em; border:0; border-radius:4px">Reset My password</button></form>`,
+    html: `Hello ${data.name}, <br> You requested for a password reset click here
+    <a href="http://localhost:3000/api/v1/auth/reset_password?token=${data.token}&email=${to}"><button type="submit" style="background:black; color:white; padding:0.5em; border:0; border-radius:4px">Reset My password</button></a>`,
     mail_settings: {
       sandbox_mode: {
         enable: process.env.NODE_ENV === "test"
       }
     },
-    // hideWarnings: true
+    hideWarnings: true
   };
   return sendGrid.send(msg).then(()=>
     okResponse(res, email, "Check your email for password reset link"))
